23c8161e2c33fdf11fe66e6192c4677338efbee8
Joao Pedro Santos <jpedro.csantos@hotmail.com>
2025-11-03 17:19:19 -0300

    fix: bug frontend sem header

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -3,6 +3,7 @@ import streamlit as st
 class ExcelValidadorUI:
     def __init__(self):
         self.set_page_config()
+        self.display_header()
 
     def set_page_config(self):
         st.set_page_config(
@@ -11,3 +12,6 @@ class ExcelValidadorUI:
             layout="wide"
         )
         st.title("Validador de Schemas Excel")
+
+    def display_header(self):
+        st.title("Insira o seu excel para validação")
