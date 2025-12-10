d216f0d7134532715732641df599656ca74679a6
Joao Pedro Santos <87459886+JpedroCSantos@users.noreply.github.com>
2025-12-09 22:41:33 -0300

    feat: adicionando observabilidade com o sentry

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,6 +1,13 @@
+import sentry_sdk
+
 from frontend import ExcelValidadorUI
 from src.backend.excel_extrator import ExcelExtractor
 from src.backend.db_conection import send_dataframe_to_database 
+
+sentry_sdk.init(
+    dsn= "https://b1e46bb04e6bf1e6b2266f52929a2e21@o4510507766382592.ingest.us.sentry.io/4510507913838592",
+    send_default_pii=True,
+)
 
 def main():
     ui = ExcelValidadorUI()
@@ -14,6 +21,7 @@ def main():
         if error:
             message = (f"Encontramos {len(error)} linhas com problemas no seu arquivo. Por favor, corrija-as e faça o upload novamente.")
             ui.display_wrong_message(message)
+            sentry_sdk.capture_message(f"Encontramos {len(error)} linhas com problemas no seu arquivo. Por favor, corrija-as e faça o upload novamente.")
             with ui.expander("Clique aqui para ver os detalhes dos erros"):
                 erros_df = extractor.get_errors_as_df(error)
                 ui.display_dataframe(erros_df)
@@ -22,6 +30,7 @@ def main():
             result = send_dataframe_to_database(df)
             if result is not None:
                 ui.display_wrong_message(result)
+            sentry_sdk.capture_message("Dados salvos com sucesso no banco de dados!")
             ui.display_success_message()
 
 if __name__ == "__main__":
